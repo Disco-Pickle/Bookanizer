34fc7dd6294c7afd5140d6d6f658a5431a6096a1
Disco-Pickle <s.sacher102@gmail.com>
2026-03-28 18:18:26 +0100

    11 - Created Migration-01-InitialMigration

---
+++ Bookanizer.REST/Bookanizer.REST/Migrations/20260328170145_Migration_01_InitialMigration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Bookanizer.REST.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_01_InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    author_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authors", x => x.author_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    username = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    book_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    isbn = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    isbn13 = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: true),
+                    country_code = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    language_code = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    average_rating = table.Column<double>(type: "double precision", nullable: false),
+                    ratings_count = table.Column<int>(type: "integer", nullable: false),
+                    author_id = table.Column<int>(type: "integer", nullable: false),
+                    num_pages = table.Column<int>(type: "integer", nullable: false),
+                    publication_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    title = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    title_without_series = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.book_id);
+                    table.ForeignKey(
+                        name: "FK_books_authors_author_id",
+                        column: x => x.author_id,
+                        principalTable: "authors",
+                        principalColumn: "author_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "interactions",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    book_id = table.Column<int>(type: "integer", nullable: false),
+                    is_read = table.Column<bool>(type: "boolean", nullable: false),
+                    rating = table.Column<double>(type: "double precision", nullable: false),
+                    date_added = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    date_updated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    read_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    started_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    read_location = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_interactions", x => new { x.user_id, x.book_id });
+                    table.ForeignKey(
+                        name: "FK_interactions_books_book_id",
+                        column: x => x.book_id,
+                        principalTable: "books",
+                        principalColumn: "book_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_interactions_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_books_author_id",
+                table: "books",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_interactions_book_id",
+                table: "interactions",
+                column: "book_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "interactions");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "authors");
+        }
+    }
+}
