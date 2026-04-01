d9cacb36b151ca1850a6f59d174f64d0ea500254
Disco-Pickle <s.sacher102@gmail.com>
2026-04-01 19:06:05 +0200

    13 - Added UserRepository

---
+++ Bookanizer.REST/Bookanizer.REST/DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Bookanizer.REST.DAL.Models;
+
+namespace Bookanizer.REST.DAL.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task CreateSingleAsync(UserModel user, CancellationToken ct = default);
+        Task<UserModel?> ReadSingleByIdAsync(string userId, CancellationToken ct = default);
+        Task UpdateSingleAsync(UserModel user, CancellationToken ct = default);
+        Task<bool> DeleteSingleByIdAsync(string id, CancellationToken ct = default);
+    }
+}
---
+++ Bookanizer.REST/Bookanizer.REST/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using Bookanizer.REST.DAL.Models;
+using Bookanizer.REST.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookanizer.REST.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        #region Constructors
+        public UserRepository(DataContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Data Context
+        private readonly DataContext _db;
+        #endregion
+
+        #region CRUD
+        public async Task CreateSingleAsync(
+            UserModel user,
+            CancellationToken ct = default)
+        {
+            var dbUser = await _db.Users.FirstOrDefaultAsync(u => u.UserId == user.UserId, ct);
+            if (dbUser == null)
+            {
+                await _db.Users.AddAsync(user, ct);
+                await _db.SaveChangesAsync(ct);
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot create user, user with this ID already exists.");
+            }
+        }
+
+        public async Task<UserModel?> ReadSingleByIdAsync(
+            string userId,
+            CancellationToken ct = default)
+        {
+            return await _db.Users.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == userId, ct);
+        }
+
+        public async Task UpdateSingleAsync(
+            UserModel user,
+            CancellationToken ct = default)
+        {
+            var dbUser = await _db.Users.FirstOrDefaultAsync(u => u.UserId == user.UserId, ct);
+            if (dbUser != null)
+            {
+                dbUser.Update(user);
+                await _db.SaveChangesAsync(ct);
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot update user, not found");
+            }
+        }
+
+        public async Task<bool> DeleteSingleByIdAsync(
+            string id,
+            CancellationToken ct = default)
+        {
+            var amountDeleted = await _db.Users.Where(u => u.UserId == id)
+                                               .ExecuteDeleteAsync(ct); // ExecuteDeleteAsync does not need a SaveChanges() call
+
+            return amountDeleted > 0;
+        }
+        #endregion
+    }
+}
