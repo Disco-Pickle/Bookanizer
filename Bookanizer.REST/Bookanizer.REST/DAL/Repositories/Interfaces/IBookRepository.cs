59dd1e4284ba9bd62217e5b320e072a9d9d3a4d0
Disco-Pickle <s.sacher102@gmail.com>
2026-04-01 18:48:51 +0200

    13 - Implemented IBookRepository

--- Bookanizer.REST/Bookanizer.REST/DAL/Repositories/Interfaces/IBookRepository.cs
+++ Bookanizer.REST/Bookanizer.REST/DAL/Repositories/Interfaces/IBookRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Bookanizer.REST.DAL.Repositories.Interfaces
+﻿using Bookanizer.REST.DAL.Models;
+
+namespace Bookanizer.REST.DAL.Repositories.Interfaces
 {
-    public class IBookRepository
+    public interface IBookRepository
     {
+        Task CreateOrUpdateSingleAsync(BookModel book, CancellationToken ct = default);
+        Task<BookModel?> ReadSingleByIdAsync(int id, CancellationToken ct = default);
+        Task<List<BookModel>> ReadMultipleByTitleAsync(string title, int skip, int take, CancellationToken ct = default);
+        Task<List<BookModel>> ReadMultipleByTitleWithoutSeriesAsync(string titleWithoutSeries, int skip, int take, CancellationToken ct = default);
+        Task<bool> DeleteSingleByIdAsync(int id, CancellationToken ct = default);
     }
 }
