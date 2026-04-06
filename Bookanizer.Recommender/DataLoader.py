cde36f26d085322a7066bb2660c0b0e9397ff69c
Disco-Pickle <s.sacher102@gmail.com>
2026-04-06 12:30:46 +0200

    16 - Added Bookanizer.Recommender

---
+++ Bookanizer.Recommender/DataLoader.py
@@ -0,0 +1,10 @@
+'''
+Loads data and prepares it for NFM processing.
+
+@references:
+X. He and T.-S. Chua, “Neural Factorization Machines for Sparse Predictive Analytics,” in Proceedings of the 40th International ACM SIGIR Conference on Research and Development in Information Retrieval, Shinjuku Tokyo Japan: ACM, Aug. 2017, pp. 355–364. doi: 10.1145/3077136.3080777.
+'''
+
+class DataLoader(object):
+    
+    
