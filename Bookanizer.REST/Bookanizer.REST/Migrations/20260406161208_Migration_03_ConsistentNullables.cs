e813ba790b7cfbb6fdcf7392118378f7fab2587c
Disco-Pickle <s.sacher102@gmail.com>
2026-04-06 18:12:53 +0200

    Hotfix - Created Migration_03_ConsistentNullables

---
+++ Bookanizer.REST/Bookanizer.REST/Migrations/20260406161208_Migration_03_ConsistentNullables.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookanizer.REST.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_03_ConsistentNullables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "rating",
+                table: "interactions",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_read",
+                table: "interactions",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "title_without_series",
+                table: "books",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ratings_count",
+                table: "books",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "num_pages",
+                table: "books",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "average_rating",
+                table: "books",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "authors",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "rating",
+                table: "interactions",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_read",
+                table: "interactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "title_without_series",
+                table: "books",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ratings_count",
+                table: "books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "num_pages",
+                table: "books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "average_rating",
+                table: "books",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "authors",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
