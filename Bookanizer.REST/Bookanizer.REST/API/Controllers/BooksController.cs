2a2ffa150249f2831e9ed691b398ef9ccbb43dc0
Disco-Pickle <s.sacher102@gmail.com>
2026-06-28 13:26:16 +0200

    23 - Added BooksController, BookReadDto, and AuthorReadDto

--- Bookanizer.REST/Bookanizer.REST/API/Controllers/BooksController.cs
+++ Bookanizer.REST/Bookanizer.REST/API/Controllers/BooksController.cs
@@ -1,16 +1,36 @@
+using Bookanizer.REST.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookanizer.REST.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class BooksController : ControllerBase
     {
-        public BooksController(ILogger<BooksController> logger)
+        #region Services
+        private readonly IBookService _bookSvc;
+        #endregion
+
+        #region Constructors
+        public BooksController(IBookService bookSvc)
         {
-            
+            _bookSvc = bookSvc;
         }
+        #endregion
 
-
+        #region Endpoints
+        [HttpGet("search")]
+        [ProducesResponseType(typeof(IEnumerable<BookReadDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Search([FromQuery] string q, CancellationToken ct = default)
+        {
+            if (string.IsNullOrWhiteSpace(q)) { return BadRequest("Empty search not allowed."); }
+            var results = await _bookSvc.SearchAsync(q, ct);
+            if (results is null) { throw new InvalidOperationException("Book search returned null unexpectedly."); }
+            return Ok(results);
+        }
+        #endregion
     }
 }
