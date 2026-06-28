7dff1a8bc61d554b029c1eb8cc727bdae8f7c677
Disco-Pickle <s.sacher102@gmail.com>
2026-06-28 14:30:29 +0200

    23 - Created CollectionController, InteractionReadDto, and InteractionUpsertDto

---
+++ Bookanizer.REST/Bookanizer.REST/API/Controllers/CollectionController.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Claims;
+using Bookanizer.REST.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookanizer.REST.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CollectionController : ControllerBase
+    {
+        #region Services
+        private readonly IInteractionService _interactionSvc;
+        #endregion
+
+        #region Constructors
+        public CollectionController(IInteractionService interactionSvc)
+        {
+            _interactionSvc = interactionSvc;
+        }
+        #endregion
+
+        #region Endpoints
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<InteractionReadDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Get(CancellationToken ct = default)
+        {
+            // Authorization via JWT
+            string? userId = GetUserIdOrNull();
+            if (userId is null) { return Unauthorized(); } // Fallback, should not be reached if Authorization worked properly
+
+            // Getting collection of interactions
+            var collection = await _interactionSvc.ReadAsync(userId, ct);
+            if (collection is null) { throw new InvalidOperationException("Collection GET returned null unexpectedly."); }
+            return Ok(collection);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Put(InteractionUpsertDto interaction, CancellationToken ct = default)
+        {
+            // Authorize via JWT
+            string? userId = GetUserIdOrNull();
+            if (userId is null) { return Unauthorized(); } // Fallback, should not be reached if Authorization worked properly
+
+            // Upserting interaction
+            await _interactionSvc.UpsertAsync(userId, interaction, ct);
+            return NoContent();
+        }
+        #endregion
+
+        #region Helpers
+        private string? GetUserIdOrNull()
+        {
+            var sub = User.FindFirstValue(ClaimTypes.NameIdentifier); // or User.FindFirstValue(JwtRegisteredClaimNames.Sub) if options.MapInboundClaims = false is set in authService
+            return string.IsNullOrWhiteSpace(sub) ? null : sub;
+        }
+        #endregion
+    }
+}
---
+++ Bookanizer.REST/Bookanizer.REST/API/DTOs/InteractionUpsertDto.cs
@@ -0,0 +1,11 @@
+﻿using Bookanizer.REST.Enums;
+
+namespace Bookanizer.REST.API.DTOs;
+
+public sealed record InteractionUpsertDto(
+    int BookId,
+    bool? IsRead,
+    double? Rating,
+    DateTimeOffset? ReadAt,
+    DateTimeOffset? StartedAt,
+    ReadLocationEnum? ReadLocation);
