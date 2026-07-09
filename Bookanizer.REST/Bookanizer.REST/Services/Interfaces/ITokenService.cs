af09c1f480fe4cd907ac7f709eda1fdda02e5e4e
Disco-Pickle <s.sacher102@gmail.com>
2026-07-09 15:30:17 +0200

    29 - Added JWT and TokenService

---
+++ Bookanizer.REST/Bookanizer.REST/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Bookanizer.REST.DAL.Models;
+
+namespace Bookanizer.REST.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateJwt(UserModel user);
+    }
+}
